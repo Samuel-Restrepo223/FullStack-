22a3b2b468577dbab695da4f807513617c1a17dd
Samuel Restrepo <SamuelRestrepo@gmail.com>
2025-06-26 18:30:13 -0500

    se crea el repo

---
+++ back/src/servidor.js
@@ -0,0 +1,14 @@
+import express from "express"; //para realizar la conexion con el servidor 
+import morgan from "morgan";// monitorear solicitudes http
+
+const servidor = express();
+servidor.use(morgan("dev"));
+servidor.use(express.json());
+
+servidor.get('/', (sol, res)=>{
+    res.status(404).send("No encontrado");
+});
+
+export default servidor;
+
+
